3a89b7ab3d5690141597039c1b4d560acde72159
unio <unioblog@gmail.com>
2019-06-11 13:26:16 +0900

    Updates

---
+++ precache-manifest.971c9f7fa6e8627ff29d3ecde15792e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "618524521f64c59f5ffeb94a024b377c",
+    "url": "/wordsthatneet/index.html"
+  },
+  {
+    "revision": "737bc15ad12b7dd792fb",
+    "url": "/wordsthatneet/static/css/main.974578b4.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/wordsthatneet/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "737bc15ad12b7dd792fb",
+    "url": "/wordsthatneet/static/js/main.13268f81.chunk.js"
+  },
+  {
+    "revision": "8e7ff150aa9025f436a9",
+    "url": "/wordsthatneet/static/js/runtime~main.a1ddf4ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wordsthatneet/static/media/logo.5d5d9eef.svg"
+  }
+]);
